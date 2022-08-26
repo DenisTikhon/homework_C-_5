16bd650fcf87e6fe7cce8ffa1d0d4df1523e98e5
DenisT <denis@tikhonov.pp.ru>
2022-08-26 12:19:11 +0300

    add task34

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//задача 34
+int N = Convert.ToInt32(new Random().Next(1,10));
+int[] array1 = new int[N];
+int i = 0;
+int count = 0;
+while (i < array1.Length)
+{
+array1[i] = new Random().Next(100,999);
+if ((array1[i] % 2) == 0) {count++;}
+Console.Write(array1[i]+" ");
+i++;
+}
+Console.WriteLine("Чётных чисел "+ count);
